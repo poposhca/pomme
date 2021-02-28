7324cdec62ac6ec60c5aadb262d03f8f2b5c0c44
Jorge Joaquin Popoca Herrera <poposhca@mbp-de-jorge.lan>
2021-02-28 13:02:10 -0600

    Add express server & heroku setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, '../build/')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build/index.html'))
+});
+
+app.listen(PORT, () => {
+   console.log(`Front end server up & running in port ${PORT}`);
+});
